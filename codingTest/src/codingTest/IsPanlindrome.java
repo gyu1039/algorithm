974836b585e7587d6f428a6afb4a123a051eb6c2
Ingyu <gyu1039@naver.com>
2022-03-16 15:24:42 +0900

    2022-03-16 : 2

--- codingTest/src/codingTest/IsPanlindrome.java
+++ codingTest/src/codingTest/IsPanlindrome.java
@@ -76,17 +76,5 @@ public class IsPanlindrome {
 	
 }
 
-class ListNode {
-	int val;
-	ListNode next;
-	ListNode() {}
-	ListNode(int val) { this.val = val; }
-	ListNode(int val, ListNode next) { this.val = val; this.next = next;}
-//	
-//	@Override
-//	public String toString() {
-//		return Integer.toString(val);
-//	}
-}
 
 
