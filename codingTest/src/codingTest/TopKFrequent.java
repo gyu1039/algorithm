0a7cd82deafd9426f83eca352fcd93aef7400d29
Ingyu <gyu1039@naver.com>
2022-03-18 15:51:41 +0900

    2022-03-18 : 1

---
+++ codingTest/src/codingTest/TopKFrequent.java
@@ -0,0 +1,34 @@
+package codingTest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TopKFrequent{
+
+	
+	public int[] topKFrequent(int[] nums, int k) {
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(
+				((Integer a, Integer b) -> map.get(b) - map.get(a)));
+		
+		int[] result = new int[k];
+		
+		for(int n : nums) {
+			if(!map.containsKey(n)) map.put(n, 1);
+			else map.put(n, map.get(n) + 1);
+		}
+		
+		for(Integer num : map.keySet()) {
+			priorityQueue.add(num);
+		}
+		
+		for(int i=0; i<k; i++) {
+			
+			result[i] = priorityQueue.poll();
+		}
+		
+		return result;
+	}
+}
