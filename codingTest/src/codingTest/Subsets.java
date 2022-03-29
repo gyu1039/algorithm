78f766fafc6061d2e1a252f58fa9e7387431f7ff
Ingyu <gyu1039@naver.com>
2022-03-29 11:16:17 +0900

    2022-03-29 : 1

--- codingTest/src/codingTest/Subsets.java
+++ codingTest/src/codingTest/Subsets.java
@@ -1,5 +1,29 @@
 package codingTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subsets {
+
+	public List<List<Integer>> subsets(int[] nums) {
+		
+		List<List<Integer>> result = new ArrayList<>();
+		
+		dfs(result, 0, new ArrayList<>(), nums);
+		
+		return result;
+    }
+	
+	public void dfs(List<List<Integer>> result, int index, List<Integer> subset, int[] nums) {
+		
+		result.add(subset);
+		
+		for(int i=index; i<nums.length; i++) {
+			subset.add(nums[i]);
+			dfs(result, i+1, new ArrayList<>(subset), nums);
+			subset.remove(subset.size() - 1);
+		}
+		
+	}
 
 }
