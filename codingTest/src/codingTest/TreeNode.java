a86fbe0a3e73c89797d427138e74f0687d0cc887
Ingyu <gyu1039@naver.com>
2022-04-12 14:43:27 +0900

    2022-04-12 : Serialize and Deserialize Binary Tree

--- codingTest/src/codingTest/TreeNode.java
+++ codingTest/src/codingTest/TreeNode.java
@@ -7,9 +7,11 @@ public class TreeNode {
 	TreeNode right;
 	
 	TreeNode() {}
+	
 	TreeNode(int val) {
 		this.val = val;
 	}
+	
 	TreeNode(int val, TreeNode left, TreeNode right) {
 		this.val = val;
 		this.left = left;
