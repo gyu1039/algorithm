1f8fc7637bf66a93ac07812d8d74a3e4f213f585
Ingyu <gyu1039@naver.com>
2022-04-22 10:52:57 +0900

    2022-04-22 : FindKthLargest

---
+++ codingTest/src/codingTest/KthLargestElementInAnArray.java
@@ -0,0 +1,42 @@
+package codingTest;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+public class KthLargestElementInAnArray {
+	
+	public int findKthLargest(int[] nums, int k) {
+		
+		Arrays.sort(nums);
+		return nums[nums.length - k];
+	}
+	
+	public int soloution2(int[] nums, int k) {
+
+		List<Integer> wrapper = Arrays.stream(nums).boxed().collect(Collectors.toList());
+		Collections.sort(wrapper, Collections.reverseOrder());
+		return wrapper.get(k - 1);
+	}
+	
+
+	public int soloution3(int[] nums, int k) {
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>(
+				(Integer a, Integer b) -> (a > b ? -1 : 1));
+			
+			for(int value : nums) {
+				pq.add(value);
+			}
+			
+			for(int i=0; i<k-1; i++) {
+				pq.poll();
+			}
+			
+			return pq.poll();
+	}
+
+
+}
