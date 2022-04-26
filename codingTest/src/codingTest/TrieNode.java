25e614dbb027c6e42e6b7a0651c06ee229f2a91a
Ingyu <gyu1039@naver.com>
2022-04-26 10:50:03 +0900

    2022-04-26 : PalindromeParis

--- codingTest/src/codingTest/TrieNode.java
+++ codingTest/src/codingTest/TrieNode.java
@@ -1,12 +1,19 @@
 package codingTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrieNode {
 	
 	boolean word;
 	TrieNode[] children;
+	int index;
+	List<Integer> list;
 	
 	public TrieNode() {
 		this.word = false;
 		this.children = new TrieNode[26];
+		index = -1;
+		list = new ArrayList<>();
 	}
 }
