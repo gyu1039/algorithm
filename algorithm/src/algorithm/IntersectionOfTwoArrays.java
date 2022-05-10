fd4b9c7ba05cd5cf0867d558fda5f1b8e8fc1b88
Ingyu <gyu1039@naver.com>
2022-05-10 13:29:22 +0900

    IntersectionOfTwoArrays

---
+++ algorithm/src/algorithm/IntersectionOfTwoArrays.java
@@ -0,0 +1,61 @@
+package algorithm;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfTwoArrays {
+
+	public int[] intersection1(int[] nums1, int[] nums2) {
+
+		Arrays.sort(nums1);
+		Arrays.sort(nums2);
+		
+		int i, j;
+		i = j = 0;
+		
+		int len1 = nums1.length;
+		int len2 = nums2.length;
+		
+		Set<Integer> set = new HashSet<>();
+		
+		while(i < len1 && j < len2) {
+			if(nums1[i] > nums2[j]) j++;
+			else if(nums1[i] < nums2[j]) i++;
+			else {
+				set.add(nums1[i]);
+				i++;
+				j++;
+			}
+		}
+		
+		int idx = 0;
+		int[] result = new int[set.size()];
+		for(int s : set) {
+			result[idx++] = s;
+		}
+		
+		return result;
+	}
+	
+	public int[] intersection2(int[] nums1, int[] nums2) {
+
+		Set<Integer> set = new HashSet<>();
+		Arrays.sort(nums2);
+		
+		for(int n : nums1) {
+			int idx = Arrays.binarySearch(nums2, n); 
+			if(idx >= 0) {
+				set.add(n);
+			}
+		}
+		
+		int idx = 0;
+		int[] result = new int[set.size()];
+		for(int s : set) {
+			result[idx++] = s;
+		}
+		
+		return result;
+	}
+}
