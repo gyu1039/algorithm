eeb14724c82ad99002535a6b99da3c37163f542b
Ingyu <gyu1039@naver.com>
2022-05-11 17:05:41 +0900

    TwoSumInputArrayIsSorted

---
+++ algorithm/src/algorithm/TwoSumInputArrayIsSorted.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+public class TwoSumInputArrayIsSorted {
+	
+	public int[] twoSum(int[] numbers, int target) {
+		
+		int left = 0;
+		int right = numbers.length - 1;
+		
+		while(left < right) {
+			if(numbers[left] + numbers[right] < target) left++;
+			else if(numbers[left] + numbers[right] > target) right--;
+			else break;
+		}
+		
+		return new int[] {left + 1, right + 1};
+	}
+}
