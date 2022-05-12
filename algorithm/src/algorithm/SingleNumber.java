d85514a86240864fc2628b96ad3ac4ef86bf172e
Ingyu <gyu1039@naver.com>
2022-05-12 11:28:44 +0900

    SingleNumber

---
+++ algorithm/src/algorithm/SingleNumber.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class SingleNumber {
+
+	public int singleNumber(int[] nums) {
+		
+			int result = 0;
+			for(int i : nums) 
+				result ^= i;
+			
+			return result;
+	}
+	
+
+}
