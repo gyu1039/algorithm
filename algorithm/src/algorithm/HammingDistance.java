18d2394e17aaad1444a40a64454ef3e3fa29d3a4
Ingyu <gyu1039@naver.com>
2022-05-12 11:40:45 +0900

    HammingDistance

---
+++ algorithm/src/algorithm/HammingDistance.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+public class HammingDistance {
+	
+	public int hammingDistance(int x, int y) {
+		int b = 0;
+		int count = 0;
+		
+		b = x ^ y;
+		while(b != 0) {
+			b = b & (b-1);
+			count++;
+		}
+		
+		return count;
+		// Integer.bitCount(result)
+	}
+}
