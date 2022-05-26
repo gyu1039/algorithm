47cb8e9cff982b98ad2eeba06ea9216c4e8fdcc5
Ingyu <gyu1039@naver.com>
2022-05-26 09:22:39 +0900

    stream으로 풀어보기

--- algorithm/src/algorithm/programmers/TheHighestAndLowestRankingOfTheLotto.java
+++ algorithm/src/algorithm/programmers/TheHighestAndLowestRankingOfTheLotto.java
@@ -1,49 +1,53 @@
 package algorithm.programmers;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.LongStream;
 
 public class TheHighestAndLowestRankingOfTheLotto {
 
 	public static int[] solution(int[] lottos, int[] win_nums) {
-		
+
 		int[] answer = new int[2];
-		Map<Integer, Integer> map = new HashMap<>();
-		map.put(6, 1);
-		map.put(5, 2);
-		map.put(4, 3);
-		map.put(3, 4);
-		map.put(2, 5);
-		map.put(1, 6);
-		map.put(0, 6);
 		
+
 		int zeroCount = (int)Arrays.stream(lottos).filter(x -> x == 0).count();
-		
+
 		Arrays.sort(lottos);
 		Arrays.sort(win_nums);
-		
+
 		int matchCount = 0;
 		for(int l : lottos) {
 			if(l == 0) continue;
 			for(int w : win_nums) {
 				if(l == w) {
-					System.out.println(l + " " + w);
 					matchCount++;
 					break;
 				}
 			}
-			
+
 		}
-				
-		answer[0] = map.get(matchCount);
-		answer[1] = map.get(matchCount + zeroCount);
 		
+		
+		int highestRanking = (lottos.length + 1) - (matchCount + zeroCount);
+		int lowestRanking = (lottos.length + 1) - (matchCount); 
+		
+		answer[0] = highestRanking > 6 ? 6 : highestRanking;
+		answer[1] = lowestRanking > 6 ? 6 : lowestRanking;
+
 		return answer;
+	}
+
+	public static int[] solution2(int[] lottos, int[] winNums) {
+
+		return LongStream.of( 
+					(lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l) || l == 0).count(),
+					(lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l)).count() 
+				).mapToInt( x -> (int)(x > 6 ? x - 1 : x))
+				.toArray();
 	}
 	
 	public static void main(String[] args) {
-		solution(new int[] {44, 1, 0, 0, 31, 25},new int[] {31, 10, 45, 1, 6, 19});
-		
+		solution2(new int[] {44, 1, 0, 0, 31, 25},new int[] {31, 10, 45, 1, 6, 19});
+
 	}
 }
