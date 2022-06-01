e2e1b0d4b42f4e63541489e0dd2d73c2f288bf45
Ingyu <gyu1039@naver.com>
2022-06-01 10:41:23 +0900

    AddaNumberThatIsNotExists

---
+++ algorithm/src/algorithm/programmers/AddaNumberThatIsNotExists.java
@@ -0,0 +1,29 @@
+package algorithm.programmers;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class AddaNumberThatIsNotExists {
+
+	public int solution1(int[] numbers) {
+
+		return IntStream.range(0, 10).filter( i ->
+		!Arrays.stream(numbers).anyMatch(x -> x == i)
+				).sum();
+	}
+
+	public int solution2(int[] numbers) {
+
+		int sum = 45;
+		for (int i : numbers) 
+			sum -= i;
+		
+		
+		return sum; //  return 45-Arrays.stream(numbers).sum();
+	}
+	
+	public int solution3(int[] numbers) {
+        return IntStream.range(0, 10)
+        		.filter(i -> Arrays.stream(numbers).noneMatch(num -> i == num)).sum();
+    }
+}
