ac06cda29c23a0c2410d91c632192620c0a7ff14
Ingyu <gyu1039@naver.com>
2022-06-02 09:13:41 +0900

    TheAdditionOfNegativeAndPositive

---
+++ algorithm/src/algorithm/programmers/TheAdditionOfNegativeAndPositive.java
@@ -0,0 +1,20 @@
+package algorithm.programmers;
+
+public class TheAdditionOfNegativeAndPositive {
+
+	public int solution(int[] absolutes, boolean[] signs) {
+		int answer = 0;
+		
+		int len = signs.length;
+		
+		for(int i=0; i<len; i++) {
+			if(signs[i]) answer += absolutes[i];
+			else answer -= absolutes[i];
+			
+			// answer += absolutes[i] * (signs[i] ? 1 : -1);
+		}
+		
+		
+		return answer;
+	}
+}
