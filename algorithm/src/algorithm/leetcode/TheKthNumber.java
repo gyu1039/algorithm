8981a2a7104adbb4f662a08ded82a46009a6ef3b
Ingyu <gyu1039@naver.com>
2022-06-07 10:32:47 +0900

    TheKthNumber

---
+++ algorithm/src/algorithm/leetcode/TheKthNumber.java
@@ -0,0 +1,20 @@
+package algorithm.leetcode;
+
+import java.util.Arrays;
+
+public class TheKthNumber {
+
+	public int[] solution(int[] array, int[][] commands) {
+        
+		int[] answer = new int[commands.length];
+        
+        int i = 0;
+        for(int[] command : commands) {
+        	int[] tmp = Arrays.copyOfRange(array, command[0]-1, command[1]);
+        	Arrays.sort(tmp);
+        	answer[i++] = tmp[command[2]-1];
+        }
+        
+        return answer;
+    }
+}
