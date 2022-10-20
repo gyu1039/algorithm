95bd95d2181e19acd8565dd71a7cd073d4eadeac
Ingyu <gyu1039@naver.com>
2022-10-20 09:02:04 +0900

    ps

---
+++ algorithm/src/algorithm/baekjoon/_10000to19999/_11689.java
@@ -0,0 +1,32 @@
+package algorithm.baekjoon._10000to19999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11689 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine());
+		
+		long result = n;
+		
+		for(long p=2; p*p<=n; p++) {
+			
+			if(n % p == 0) {
+				result = result - result / p;
+				while(n % p == 0) {
+					n /= p;
+				}
+			}
+		}
+		
+		if(n > 1) {
+			result = result - result / n;
+		}
+		
+		System.out.println(result);
+	}
+}
---
+++ algorithm/src/algorithm/baekjoon/_1to9999/_1033.java
@@ -0,0 +1,9 @@
+package algorithm.baekjoon._1to9999;
+
+public class _1033 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
