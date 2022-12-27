05c9b138772416cc92004dee92b11485005b6eac
Ingyu <gyu1039@naver.com>
2022-12-28 04:14:52 +0900

    백준_단어뒤집기2

---
+++ algorithm/src/algorithm/baekjoon/_10000to99999/_17413.java
@@ -0,0 +1,55 @@
+package algorithm.baekjoon._10000to99999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class _17413 {
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        br.close();
+
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char cur = s.charAt(i);
+
+            if (cur == '<') {
+                while(!stack.isEmpty()) {
+                    sb.append(stack.pop());
+                }
+
+                int endIdx = s.indexOf('>', i);
+                sb.append(s.substring(i, endIdx+1));
+                i = endIdx;
+                continue;
+            }
+
+            if (cur == ' ') {
+                while (!stack.isEmpty()) {
+                    sb.append(stack.pop());
+                }
+                sb.append(cur);
+                continue;
+            }
+
+            stack.push(cur);
+
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb);
+    }
+}
